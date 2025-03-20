bcd0c14baa3f1ff11215b3a6ec9f834b2c622230
alena <alllena_sh@mail.ru>
2025-03-20 19:41:22 +0700

    первый коммит

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Button from './components/Button';
+import Navigation from './components/Navigation';
+
+const App = () => {
+    return (
+        <div>
+            <Navigation />
+            <h1>Hello World</h1>
+            <Button />
+        </div>
+    );
+};
+
+export default App;
